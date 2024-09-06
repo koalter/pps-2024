5c76904b5f0bc3f0ccd6d44d6d894202f2c837c4
Lorenzo Cea <heavens_drive090@hotmail.com>
2024-09-05 21:42:55 -0300

    Version funcional

--- prueba-app/src/app/home/home.page.ts
+++ prueba-app/src/app/home/home.page.ts
@@ -1,19 +1,23 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonButtons, IonIcon } from '@ionic/angular/standalone';
 import { AuthService } from '../services/auth.service';
 import { Router } from '@angular/router';
+import { addIcons } from 'ionicons';
+import { arrowBackOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonIcon, IonButtons, IonButton, IonHeader, IonToolbar, IonTitle, IonContent],
 })
 export class HomePage {
   constructor(private authService: AuthService,
     private router: Router
-  ) {}
+  ) {
+    addIcons({arrowBackOutline});
+  }
 
   logout() {
     this.authService.logout()
--- prueba-app/src/app/login/login.page.ts
+++ prueba-app/src/app/login/login.page.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton, IonGrid, IonCol, IonRow, IonItem, IonLabel, IonList, IonInput, IonText, IonIcon } from '@ionic/angular/standalone';
 import { Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 
@@ -10,26 +10,27 @@ import { AuthService } from '../services/auth.service';
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, ReactiveFormsModule]
+  imports: [IonIcon, IonText, IonInput, IonList, IonLabel, IonItem, IonRow, IonCol, IonGrid, IonButton, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, ReactiveFormsModule]
 })
 export class LoginPage {
   form = this.fb.group({
     correo: ['', [Validators.required, Validators.email]],
-    clave: ['', [Validators.required]]
+    clave: ['', [Validators.required, Validators.minLength(8)]]
   });
-  trigger = false;
   
   constructor(private fb: FormBuilder,
     private router: Router,
     private authService: AuthService) {}
 
   onSubmit() {
-    this.trigger = true;
+    this.fillTestData();
+
     if (this.form.valid) {
       this.authService.login(this.form.value.correo!, this.form.value.clave!)
         .subscribe({
           next: () => {
-            this.router.navigateByUrl('/home');
+            this.router.navigateByUrl('/home')
+              .then(() => this.form.reset());
           },
           error: (err) => {
             console.log(err);
@@ -38,5 +39,30 @@ export class LoginPage {
     } else {
       console.log("Formulario no válido");
     }
+  }
+
+  correo_getError() {
+    if (this.form.controls.correo.hasError('required')) {
+      return 'El correo electrónico es requerido';
+    }
+    if (this.form.controls.correo.hasError('email')) {
+      return 'El correo electrónico no es válido';
+    }
+    return '';
+  }
+
+  clave_getError() {
+    if (this.form.controls.clave.hasError('required')) {
+      return 'La clave es requerida';
+    }
+    if (this.form.controls.clave.hasError('minlength')) {
+      return 'La clave debe contener al menos 8 caracteres';
+    }
+    return '';
+  }
+
+  fillTestData() {
+    this.form.get('correo')?.setValue('iiqwjerbpuzbmyctvu@nbmbb.com');
+    this.form.get('clave')?.setValue('iiqwjerbpuzbmyctvu@nbmbb.com');
   }
 }
